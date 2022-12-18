a02a2e1a7377ec0670133a02f7569b76eb7ef8ce
AnthonyPowell <anthonydewpow@gmail.com>
2022-12-18 10:46:26 -0600

    commit tree 45807f23e64327287a71e4f31e1d4e3c2fb76e14

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(play = "roller-skate"){
+  return(`This Saturday, I want to ${play}!`);
+}
+saturdayFun();
+
+
+const mondayWork = function (work = "go to the office") {
+    return(`This Monday, I will ${work}.`);
+}
+
+function wrapAdjective (not = '*') {
+ return function(name='special') {
+    return `You are ${not}${name}${not}!`
+    }
+}
